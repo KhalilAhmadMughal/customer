092b31bbbed982d1e5de7ee577e87eeafc66ba0c
KhalilAhmadMughal <khalilahmadm0@gmail.com>
2023-01-27 18:53:21 +0500

    used async await and type aliase

--- dist/indexApp.js
+++ dist/indexApp.js
@@ -1,41 +1,44 @@
 "use strict";
 const api_url = "http://localhost:3000/customers";
-function getUserDataFromDatabase() {
-    return fetch(api_url).then(data => data.json());
-}
-getUserDataFromDatabase().then(data => {
+async function getDataFromDatabase(url) {
+    let Response = await fetch(url);
+    let data = await Response.json();
     showCustomerProfile(data);
-});
+}
 function showCustomerProfile(data) {
     const customerContainer = document.getElementById("customer-container");
     data.forEach(function (element) {
-        let customer = document.createElement("div");
-        customer.innerHTML = element.name;
-        customer.id = element.id;
-        customer.classList.add("customer");
-        customer.addEventListener("click", function () {
-            showCustomersDetails(element.id, element);
+        let customerElemet = document.createElement("div");
+        customerElemet.innerHTML = element.name;
+        customerElemet.id = "customer" + element.id;
+        customerElemet.classList.add("customer");
+        customerElemet.addEventListener("click", function () {
+            showCustomersDetails(element);
         });
-        customerContainer === null || customerContainer === void 0 ? void 0 : customerContainer.appendChild(customer);
+        customerContainer.appendChild(customerElemet);
     });
 }
-function showCustomersDetails(id, data) {
+function showCustomersDetails(data) {
     const detailContainer = document.getElementById("detail-box");
     detailContainer.innerHTML = "";
     let heading = document.createElement("h2");
     heading.innerHTML = "Customer- " + data.id + " -Details";
     detailContainer === null || detailContainer === void 0 ? void 0 : detailContainer.appendChild(heading);
-    getDetailSectionComponent().then(component => {
+    getDetailSectionComponent().then((component) => {
         let html;
         const lenght = Object.keys(data).length;
         for (let i = 0; i < lenght; i++) {
-            html = component.replace("$title", Object.keys(data)[i])
+            html = component
+                .replace("$title", Object.keys(data)[i])
                 .replace("$content", data[Object.keys(data)[i]]);
             detailContainer.innerHTML += html;
         }
     });
     detailContainer.style.display = "block";
 }
-function getDetailSectionComponent() {
-    return fetch("../dist/detailSection.html").then(Response => Response.text());
+async function getDetailSectionComponent() {
+    let Response = await fetch("../dist/detailSection.html");
+    let component = await Response.text();
+    return component;
 }
+getDataFromDatabase(api_url);
--- src/indexApp.ts
+++ src/indexApp.ts
@@ -1,46 +1,66 @@
 const api_url: string = "http://localhost:3000/customers";
 
-function getUserDataFromDatabase(): Promise<any> {
-  return fetch(api_url).then(data=>data.json())
-}
-getUserDataFromDatabase().then(data=>{
+type Customer = {
+  id: number;
+  username: string;
+  password: string;
+  name: string;
+  father_name: string;
+  age: number;
+  isAdmin: boolean;
+  cnic: number;
+};
+
+async function getDataFromDatabase(url: string): Promise<void> {
+  let Response = await fetch(url);
+  let data: Array<Customer> = await Response.json();
   showCustomerProfile(data);
-})
-
-function showCustomerProfile(data:any) {
-  const customerContainer = document.getElementById("customer-container");
-      data.forEach(function(element:any) {
-      let customer = document.createElement("div");
-      customer.innerHTML=element.name;
-      customer.id=element.id;
-      customer.classList.add("customer");
-      customer.addEventListener("click", function(){
-        showCustomersDetails(element.id, element);
-      })
-      customerContainer?.appendChild(customer);
-    }); 
 }
 
-function showCustomersDetails(id: number, data:any){
-  const detailContainer: HTMLElement = document.getElementById("detail-box") as HTMLElement;
-  detailContainer.innerHTML=""
+function showCustomerProfile(data: Array<Customer>) {
+  const customerContainer: HTMLElement = document.getElementById(
+    "customer-container"
+  ) as HTMLElement;
+
+  data.forEach(function (element: Customer) {
+    let customerElemet = document.createElement("div");
+    customerElemet.innerHTML = element.name;
+    customerElemet.id = "customer" + element.id;
+    customerElemet.classList.add("customer");
+    customerElemet.addEventListener("click", function () {
+      showCustomersDetails(element);
+    });
+    customerContainer.appendChild(customerElemet);
+  });
+}
+
+function showCustomersDetails(data: any) {
+  const detailContainer: HTMLElement = document.getElementById(
+    "detail-box"
+  ) as HTMLElement;
+  detailContainer.innerHTML = "";
   let heading = document.createElement("h2");
   heading.innerHTML = "Customer- " + data.id + " -Details";
   detailContainer?.appendChild(heading);
 
-  getDetailSectionComponent().then(component => {
+  getDetailSectionComponent().then((component: string) => {
     let html: string;
-    const lenght: number= Object.keys(data).length;
-    for(let i:number=0;i<lenght;i++){
-      html = component.replace("$title", Object.keys(data)[i])
-      .replace("$content",data[Object.keys(data)[i]]);
-      detailContainer.innerHTML +=html;
+    const lenght: number = Object.keys(data).length;
+    for (let i: number = 0; i < lenght; i++) {
+      html = component
+        .replace("$title", Object.keys(data)[i])
+        .replace("$content", data[Object.keys(data)[i]]);
+      detailContainer.innerHTML += html;
     }
   });
   detailContainer.style.display = "block";
 }
 
-function getDetailSectionComponent(){
-  return fetch("../dist/detailSection.html").then(Response=>Response.text());
+async function getDetailSectionComponent() {
+  let Response = await fetch("../dist/detailSection.html");
+  let component: string = await Response.text();
+  return component;
 }
 
+//function calls
+getDataFromDatabase(api_url);
